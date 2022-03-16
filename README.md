afdf70af887e08731ec9e71613eb7c9db9852a31
Bill Allen <ballen@spectric.com>
2022-03-16 15:04:12 -0400

    Added license info.

---
+++ README.md
@@ -0,0 +1,3 @@
+# georgia
+
+Fast **geo** **R**ust helper functions for Python.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+/*
+ * File: lib.rs
+ * Copyright (c) 2022, Spectric Labs Inc., All rights reserved.
+ *
+ * This file is part of the georgia Python package.
+ *
+ * Licensed to the Spectric Labs (Spectric) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  Spectric licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 
@@ -5,16 +29,8 @@ const EARTH_RADIUS_METERS: f32 = 6_371_009.0;  // https://en.wikipedia.org/wiki/
 const EPSILON32: f32 = 1.0e-9;
 const PI32: f32 = 3.141592653589793238462643383279502884;
 
-fn valid_latitude(lat: f32) -> bool {
-    lat > -90.0 && lat < 90.0
-}
-
-fn valid_longitude(lon: f32) -> bool {
-    lon > -180.0 && lon < 180.0
-}
-
 fn valid_coordinates(lon: f32, lat: f32) -> bool {
-    valid_longitude(lon) && valid_latitude(lat)
+    lat > -90.0 && lat < 90.0 && lon > -180.0 && lon < 180.0
 }
 
 #[pyfunction]
