d6fad81f9ade90aeb7e19dc7945a7cad454d24f4
Bill Allen <ballen@spectric.com>
2024-09-02 14:28:45 -0400

    Switched from f32 to f64 for greater precision over large areas.

--- src/lib.rs
+++ src/lib.rs
@@ -22,16 +22,16 @@
  * under the License.
  */
 
-use std::f32::consts::PI;
+use std::f64::consts::PI;
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 
-const EARTH_RADIUS_METERS: f32 = 6_371_008.8;  // IUGG mean Earth radius
-const EPSILON32: f32 = 1.0e-9;
+const EARTH_RADIUS_METERS: f64 = 6_371_008.8;  // IUGG mean Earth radius
+const EPSILON32: f64 = 1.0e-9;
 
 /// Raises a Python ValueError if the coordinates are not valid.
 #[pyfunction]
-fn valid_coordinates(lon: f32, lat: f32) -> PyResult<()> {
+fn valid_coordinates(lon: f64, lat: f64) -> PyResult<()> {
     if !(-90.0..=90.0).contains(&lat) || !(-180.0..=180.0).contains(&lon) {
         return Err(PyValueError::new_err("invalid coordinates"));
     }
@@ -49,7 +49,7 @@ fn valid_coordinates(lon: f32, lat: f32) -> PyResult<()> {
 /// * `lat2` - The latitude of the end location.
 /// * `radius` - The radius of the sphere in meters.
 #[pyfunction]
-fn great_circle_distance_with_radius(lon1: f32, lat1: f32, lon2: f32, lat2: f32, radius: f32) -> PyResult<f32> {
+fn great_circle_distance_with_radius(lon1: f64, lat1: f64, lon2: f64, lat2: f64, radius: f64) -> PyResult<f64> {
     valid_coordinates(lon1, lat1)?;
     valid_coordinates(lon2, lat2)?;
 
@@ -70,7 +70,7 @@ fn great_circle_distance_with_radius(lon1: f32, lat1: f32, lon2: f32, lat2: f32,
     let cos_delta_lon = delta_lon_rad.cos();
     let sin_delta_lon = delta_lon_rad.sin();
 
-    let d = f32::atan2(f32::sqrt((cos_lat2 * sin_delta_lon).powi(2) +
+    let d = f64::atan2(f64::sqrt((cos_lat2 * sin_delta_lon).powi(2) +
              (cos_lat1 * sin_lat2 -
               sin_lat1 * cos_lat2 * cos_delta_lon).powi(2)),
               sin_lat1 * sin_lat2 + cos_lat1 * cos_lat2 * cos_delta_lon);
@@ -86,7 +86,7 @@ fn great_circle_distance_with_radius(lon1: f32, lat1: f32, lon2: f32, lat2: f32,
 /// * `lon2` - The longitude of the end location.
 /// * `lat2` - The latitude of the end location.
 #[pyfunction]
-fn great_circle_distance(lon1: f32, lat1: f32, lon2: f32, lat2: f32) -> PyResult<f32> {
+fn great_circle_distance(lon1: f64, lat1: f64, lon2: f64, lat2: f64) -> PyResult<f64> {
     great_circle_distance_with_radius(lon1, lat1, lon2, lat2, EARTH_RADIUS_METERS)
 }
 
@@ -100,23 +100,23 @@ fn great_circle_distance(lon1: f32, lat1: f32, lon2: f32, lat2: f32) -> PyResult
 /// * `distance` - The distance in meters.
 /// * `radius` - The radius in meters.
 #[pyfunction]
-fn line_of_bearing_with_radius(lon: f32, lat: f32, bearing: f32, distance: f32, radius: f32) -> PyResult<(f32, f32)> {
+fn line_of_bearing_with_radius(lon: f64, lat: f64, bearing: f64, distance: f64, radius: f64) -> PyResult<(f64, f64)> {
     valid_coordinates(lon, lat)?;
 
-    let rad_bearing = f32::to_radians(bearing);
-    let rad_lat = f32::to_radians(lat);
-    let rad_lon = f32::to_radians(lon);
+    let rad_bearing = f64::to_radians(bearing);
+    let rad_lat = f64::to_radians(lat);
+    let rad_lon = f64::to_radians(lon);
     let d_div_r = distance / radius;
-    let rad_new_lat = f32::asin(
-        f32::sin(rad_lat)*f32::cos(d_div_r) +
-        f32::cos(rad_lat)*f32::sin(d_div_r)*f32::cos(rad_bearing)
+    let rad_new_lat = f64::asin(
+        f64::sin(rad_lat)*f64::cos(d_div_r) +
+        f64::cos(rad_lat)*f64::sin(d_div_r)*f64::cos(rad_bearing)
     );
-    let rad_new_lon = rad_lon + f32::atan2(
-        f32::sin(rad_bearing)*f32::sin(d_div_r)*f32::cos(rad_lat),
-        f32::cos(d_div_r)-f32::sin(rad_lat)*f32::sin(rad_new_lat)
+    let rad_new_lon = rad_lon + f64::atan2(
+        f64::sin(rad_bearing)*f64::sin(d_div_r)*f64::cos(rad_lat),
+        f64::cos(d_div_r)-f64::sin(rad_lat)*f64::sin(rad_new_lat)
     );
-    let new_lat = f32::to_degrees(rad_new_lat);
-    let new_lon = f32::to_degrees(rad_new_lon);
+    let new_lat = f64::to_degrees(rad_new_lat);
+    let new_lon = f64::to_degrees(rad_new_lon);
     Ok((new_lon, new_lat))
 }
 
@@ -129,7 +129,7 @@ fn line_of_bearing_with_radius(lon: f32, lat: f32, bearing: f32, distance: f32, 
 /// * `bearing` - The bearing to follow in degrees, clockwise from north.
 /// * `distance` - The distance in meters.
 #[pyfunction]
-fn line_of_bearing(lon: f32, lat: f32, bearing: f32, distance: f32) -> PyResult<(f32, f32)> {
+fn line_of_bearing(lon: f64, lat: f64, bearing: f64, distance: f64) -> PyResult<(f64, f64)> {
     line_of_bearing_with_radius(lon, lat, bearing, distance, EARTH_RADIUS_METERS)
 }
 
@@ -148,7 +148,7 @@ fn line_of_bearing(lon: f32, lat: f32, bearing: f32, distance: f32) -> PyResult<
 /// * `lat` - The latitude of the starting point.
 /// * `distance_meters` - The distance to each side of the bounding box from the starting point.
 #[pyfunction]
-fn bounding_box_for_point(lon: f32, lat: f32, distance_meters: f32) -> PyResult<(f32, f32, f32, f32)> {
+fn bounding_box_for_point(lon: f64, lat: f64, distance_meters: f64) -> PyResult<(f64, f64, f64, f64)> {
     valid_coordinates(lon, lat)?;
 
     if distance_meters < 0.0 {
@@ -156,19 +156,19 @@ fn bounding_box_for_point(lon: f32, lat: f32, distance_meters: f32) -> PyResult<
     }
 
     let dist_radians = distance_meters / EARTH_RADIUS_METERS;
-    let lon_rad = f32::to_radians(lon);
-    let lat_rad = f32::to_radians(lat);
-    let delta_lon_rad = f32::asin(f32::sin(dist_radians) / f32::cos(lat_rad));
+    let lon_rad = f64::to_radians(lon);
+    let lat_rad = f64::to_radians(lat);
+    let delta_lon_rad = f64::asin(f64::sin(dist_radians) / f64::cos(lat_rad));
 
     let north_rad = lat_rad + dist_radians;
     let south_rad = lat_rad - dist_radians;
     let east_rad = lon_rad + delta_lon_rad;
     let west_rad = lon_rad - delta_lon_rad;
 
-    let bbox_north = f32::to_degrees(north_rad);
-    let bbox_south = f32::to_degrees(south_rad);
-    let bbox_east = f32::to_degrees(east_rad);
-    let bbox_west = f32::to_degrees(west_rad);
+    let bbox_north = f64::to_degrees(north_rad);
+    let bbox_south = f64::to_degrees(south_rad);
+    let bbox_east = f64::to_degrees(east_rad);
+    let bbox_west = f64::to_degrees(west_rad);
 
     Ok((
         restrict_longitude(bbox_west),
@@ -187,11 +187,11 @@ fn bounding_box_for_point(lon: f32, lat: f32, distance_meters: f32) -> PyResult<
 /// * x - Tile x coordinate
 /// * y - Tile y coordinate
 /// * z - Tile zoom level
-fn _wm_upper_left(x: u32, y: u32, z: u32) -> (f32, f32) {
+fn _wm_upper_left(x: u32, y: u32, z: u32) -> (f64, f64) {
     // useful link: https://www.maptiler.com/google-maps-coordinates-tile-bounds-projection/
-    let inv_z2: f32 = 1.0 / 2.0_f32.powi(z as i32);
-    let lon = (x as f32) * inv_z2 * 360.0 - 180.0;
-    let lat = f32::to_degrees(f32::atan(f32::sinh(PI * (1.0 - 2.0 * (y as f32) * inv_z2))));
+    let inv_z2: f64 = 1.0 / 2.0_f64.powi(z as i32);
+    let lon = (x as f64) * inv_z2 * 360.0 - 180.0;
+    let lat = f64::to_degrees(f64::atan(f64::sinh(PI * (1.0 - 2.0 * (y as f64) * inv_z2))));
     (lon, lat)
 }
 
@@ -203,7 +203,7 @@ fn _wm_upper_left(x: u32, y: u32, z: u32) -> (f32, f32) {
 /// * y - Tile y coordinate
 /// * z - Tile zoom level
 #[pyfunction]
-fn wm_upper_left(x: u32, y: u32, z:u32) -> PyResult<(f32, f32)> {
+fn wm_upper_left(x: u32, y: u32, z:u32) -> PyResult<(f64, f64)> {
     Ok(_wm_upper_left(x, y, z))
 }
 
@@ -222,7 +222,7 @@ fn wm_upper_left(x: u32, y: u32, z:u32) -> PyResult<(f32, f32)> {
 /// * x - Tile x coordinate
 /// * y - Tile y coordinate
 /// * z - Tile zoom level
-fn _wm_bounds(x: u32, y: u32, z: u32) -> (f32, f32, f32, f32) {
+fn _wm_bounds(x: u32, y: u32, z: u32) -> (f64, f64, f64, f64) {
     let a = _wm_upper_left(x, y, z);
     let b = _wm_upper_left(x+1, y+1, z);
     (a.0, b.1+EPSILON32, b.0-EPSILON32, a.1) // west, south, east, north
@@ -240,7 +240,7 @@ fn _wm_bounds(x: u32, y: u32, z: u32) -> (f32, f32, f32, f32) {
 /// * y - Tile y coordinate.
 /// * z - Tile zoom level.
 #[pyfunction]
-fn wm_bounds(x: u32, y: u32, z: u32) -> PyResult<(f32, f32, f32, f32)> {
+fn wm_bounds(x: u32, y: u32, z: u32) -> PyResult<(f64, f64, f64, f64)> {
     Ok(_wm_bounds(x, y, z))
 }
 
@@ -252,7 +252,7 @@ fn wm_bounds(x: u32, y: u32, z: u32) -> PyResult<(f32, f32, f32, f32)> {
 /// * south - Southern latitude boundary.
 /// * east - Eastern longitude boundary.
 /// * norht - Northern latitude boundary.
-fn tile_center_lon_lat(west: f32, south: f32, east: f32, north: f32) -> (f32, f32) {
+fn tile_center_lon_lat(west: f64, south: f64, east: f64, north: f64) -> (f64, f64) {
     ((west + east) / 2.0, (south + north) / 2.0)
 }
 
@@ -264,7 +264,7 @@ fn tile_center_lon_lat(west: f32, south: f32, east: f32, north: f32) -> (f32, f3
 ///
 /// # Arguments
 /// * lon - The longitude to restrict.
-fn restrict_longitude(lon: f32) -> f32 {
+fn restrict_longitude(lon: f64) -> f64 {
     if lon < -180.0 {
         -180.0
     } else if lon > 180.0 {
@@ -282,7 +282,7 @@ fn restrict_longitude(lon: f32) -> f32 {
 ///
 /// # Arguments
 /// * lat - The latitude to restrict.
-fn restrict_latitude(lat: f32) -> f32 {
+fn restrict_latitude(lat: f64) -> f64 {
     if lat < -90.0 {
         -90.0
     } else if lat > 90.0 {
@@ -313,7 +313,7 @@ fn restrict_latitude(lat: f32) -> f32 {
 /// * z - Tile zoom level.
 /// * expansion_meters - Number of meters to expand the bounding box beyond the tile boundaries.
 #[pyfunction]
-fn wm_tile_expanded_bbox(x: u32, y: u32, z: u32, expansion_meters: f32) -> PyResult<(f32, f32, f32, f32)> {
+fn wm_tile_expanded_bbox(x: u32, y: u32, z: u32, expansion_meters: f64) -> PyResult<(f64, f64, f64, f64)> {
     assert!(expansion_meters >= 0.0);
     // based on http://janmatuschek.de/LatitudeLongitudeBoundingCoordinates
     let (tile_west, tile_south, tile_east, tile_north) = _wm_bounds(x, y, z);
@@ -324,21 +324,21 @@ fn wm_tile_expanded_bbox(x: u32, y: u32, z: u32, expansion_meters: f32) -> PyRes
     let search_radians = search_meters / EARTH_RADIUS_METERS;
 
     let (tile_center_lon, tile_center_lat) = tile_center_lon_lat(tile_west, tile_south, tile_east, tile_north);
-    let tile_center_lon_rad = f32::to_radians(tile_center_lon);
-    let tile_center_lat_rad = f32::to_radians(tile_center_lat);
-    let delta_lon_rad = f32::asin(f32::sin(search_radians) / f32::cos(tile_center_lat_rad));
+    let tile_center_lon_rad = f64::to_radians(tile_center_lon);
+    let tile_center_lat_rad = f64::to_radians(tile_center_lat);
+    let delta_lon_rad = f64::asin(f64::sin(search_radians) / f64::cos(tile_center_lat_rad));
 
     let north_rad = tile_center_lat_rad + search_radians;
     let south_rad = tile_center_lat_rad - search_radians;
     let east_rad = tile_center_lon_rad + delta_lon_rad;
     let west_rad = tile_center_lon_rad - delta_lon_rad;
 
-    let bbox_north = f32::to_degrees(north_rad);
-    let bbox_south = f32::to_degrees(south_rad);
+    let bbox_north = f64::to_degrees(north_rad);
+    let bbox_south = f64::to_degrees(south_rad);
 
     // have to handle case where delta_lon_rad is NaN, like when x=0, y=0, z=1, expansion_meters=1
-    let bbox_east = if !delta_lon_rad.is_nan() { f32::to_degrees(east_rad) } else { tile_east };
-    let bbox_west = if !delta_lon_rad.is_nan() { f32::to_degrees(west_rad) } else { tile_west };
+    let bbox_east = if !delta_lon_rad.is_nan() { f64::to_degrees(east_rad) } else { tile_east };
+    let bbox_west = if !delta_lon_rad.is_nan() { f64::to_degrees(west_rad) } else { tile_west };
 
     Ok((
         restrict_longitude(bbox_west),
